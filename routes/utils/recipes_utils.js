d2d981e1ac49934a7dfb46edac08236aceaf3efc
nettamei <nettamei@post.bgu.ac.il>
2024-09-03 10:53:01 +0300

    search

--- routes/utils/recipes_utils.js
+++ routes/utils/recipes_utils.js
@@ -13,7 +13,7 @@ async function getRecipeInformation(recipe_id) {
             includeNutrition: false,
             //apiKey: process.env.spooncular_apiKey
             // apiKey: "093ccbf0ecfa4b0a8e68366711ced8cf"
-            apiKey: "7c5f0b669948478e8d4909daa8e75f4d"
+            apiKey: "cd498151481f40dda0cc39d8a1f34b29"
         }
     }); 
     let { id, title, readyInMinutes, image, vegan, vegetarian, glutenFree, instructions, extendedIngredients } = recipe_info.data;
@@ -37,7 +37,7 @@ async function getRecipeDetails(recipe_id) {
             includeNutrition: false,
             //apiKey: process.env.spooncular_apiKey
             // apiKey: "093ccbf0ecfa4b0a8e68366711ced8cf"
-            apiKey: "7c5f0b669948478e8d4909daa8e75f4d"
+            apiKey: "cd498151481f40dda0cc39d8a1f34b29"
         }
     }); 
     let { id, title, readyInMinutes, image, aggregateLikes, vegan, vegetarian, glutenFree } = recipe_info.data;
@@ -63,7 +63,7 @@ async function searchRecipe(recipeName, cuisine, diet, intolerance, number) {
             number: number,
             //apiKey: process.env.spooncular_apiKey
             // apiKey: "093ccbf0ecfa4b0a8e68366711ced8cf"
-            apiKey: "7c5f0b669948478e8d4909daa8e75f4d"
+            apiKey: "cd498151481f40dda0cc39d8a1f34b29"
         }
     });
 
@@ -73,34 +73,6 @@ async function searchRecipe(recipeName, cuisine, diet, intolerance, number) {
     return recipesDetails;
 }
 
-// async function searchRecipe(recipeName, cuisine, diet, intolerance, number) {
-//     try {
-//         console.log("Search Parameters:", {
-//             query: recipeName,
-//             cuisine: cuisine,
-//             diet: diet,
-//             intolerances: intolerance,
-//             number: number,
-//         });
-
-//         const response = await axios.get(`${api_domain}/complexSearch`, {
-//             params: {
-//                 query: recipeName,
-//                 cuisine: cuisine,
-//                 diet: diet,
-//                 intolerances: intolerance,
-//                 number: number,
-//                 apiKey: "0239d1c9dda348c6902fdc04c32c40ae"
-//             }
-//         });
-
-//         console.log("API Response:", response.data);
-//         return response.data;
-//     } catch (error) {
-//         console.error("Error in searchRecipe:", error);
-//         throw error;  // Re-throw the error so it can be handled elsewhere
-//     }
-// }
 
 async function randomRecipes(limit) {
     const response = await axios.get(`${api_domain}/random`, {
@@ -108,7 +80,7 @@ async function randomRecipes(limit) {
             number: limit,
             //apiKey: process.env.spooncular_apiKey
             // apiKey: "093ccbf0ecfa4b0a8e68366711ced8cf"
-            apiKey: "7c5f0b669948478e8d4909daa8e75f4d"
+            apiKey: "cd498151481f40dda0cc39d8a1f34b29"
 
         }
     });
